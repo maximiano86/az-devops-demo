9a153a1c925c99a0c51de5d8cfbdc42fbb7a7c7c
maximiano86 <maximiano.alejandro@gmail.com>
2021-05-03 17:26:33 -0500

    chore: update a couple of files

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     sign = sample_function('!',3)
-    return f'Hello, Azure DevOps{sing}'
+    return f'Hello, Azure DevOps{sign}'
 
 def sample_function(str1, num2):
     return str1 * num2
