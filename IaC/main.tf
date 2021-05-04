14fb885d7db5315d81b8e39b9514b30cf8281bbc
maximiano86 <maximiano.alejandro@gmail.com>
2021-05-04 10:40:13 -0500

    add: testing terraform

--- IaC/main.tf
+++ IaC/main.tf
@@ -1,20 +1,20 @@
 terraform {
-  required_providers{
+  required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>2.0"
     }
   }
   backend "azurerm" {
-    resource_group_name   = "tstate"
-    storage_account_name  = "tstate11753"
-    container_name        = "tstate"
-    key                   = "terraform.tfstate"
+    resource_group_name  = "tstate"
+    storage_account_name = "tstate11753"
+    container_name       = "tstate"
+    key                  = "terraform.tfstate"
   }
 }
 
 # Configure the Azure provider
-provider "azurerm" { 
+provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x. 
   # If you are using version 1.x, the "features" block is not allowed.
   features {}
